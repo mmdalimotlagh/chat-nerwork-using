d142952d2ce146748b4ec86349ea128d56d81b72
mmdalimotlagh <147492410+mmdalimotlagh@users.noreply.github.com>
2024-04-24 21:24:54 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ChatServer
+{
+    private static List<TcpClient> clients = new List<TcpClient>();
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 8888);
+        server.Start();
+        Console.WriteLine("Server started on port 8888");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            clients.Add(client);
+            Console.WriteLine("Client connected");
+
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received: " + message);
+
+            BroadcastMessage(message, client);
+        }
+
+        client.Close();
+    }
+
+    static void BroadcastMessage(string message, TcpClient sender)
+    {
+        foreach (TcpClient client in clients)
+        {
+            if (client != sender)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] response = Encoding.ASCII.GetBytes(message);
+                stream.Write(response, 0, response.Length);
+            }
+        }
+    }
+}
---
+++ client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class ChatClient
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient();
+        client.Connect(IPAddress.Parse("127.0.0.1"), 8888);
+        Console.WriteLine("Connected to server");
+
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            Console.Write("Enter message: ");
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+
+            byte[] responseBuffer = new byte[1024];
+            int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+            string response = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+            Console.WriteLine("Server response: " + response);
+        }
+
+        client.Close();
+    }
+}
